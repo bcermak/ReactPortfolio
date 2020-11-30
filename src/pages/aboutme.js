2fe82d25f4d30c041392d7409eb597b451863ac5
Brandon Cermak <cermakprofessional@gmail.com>
2020-11-30 17:41:31 -0600

    cracked the code on images in react - first image for AboutMe in this commit

--- src/pages/aboutme.js
+++ src/pages/aboutme.js
@@ -1,15 +1,18 @@
 import React from "react";
+import Image from 'react-bootstrap/Image'
 import 'bootstrap/dist/css/bootstrap.min.css';
+import linkedin1 from './Assets/Images/linkedin1.png'
 
 
 
 function AboutMe() {
     return <div>
+       
         <div className ="row">
             <div className ="col-md-6">
                 <div className = "card-body">
                 <h2 className = "knowMe"> Get to Know Brandon </h2>
-                <img src=""alt="LinkedIn"></img>
+                <Image src={linkedin1}></Image>
                 <p className = "aboutMe">I am currently in the process of a major career change after a very unexpected start to my 2020. Hospitality is/was my passion, however, there was always the idea in the back of my mind to make a major career shift to a more ‘technical’ occupation...after some motivation from the events of COVID19, I am ecstatic to be on my way to becoming a Full Stack Web Developer!</p>
                 </div>
             </div>
