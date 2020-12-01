a33968d937dd7085336be24d1b7e3f906fc8ac3b
Brandon Cermak <cermakprofessional@gmail.com>
2020-11-30 20:56:26 -0600

    reworking AboutMe. Adding card/reformatting

--- src/pages/aboutme.js
+++ src/pages/aboutme.js
@@ -2,13 +2,18 @@ import React from "react";
 import Image from 'react-bootstrap/Image'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import linkedin1 from './Assets/Images/linkedin1.png'
+import { Card, CardTitle, CardText } from 'reactstrap';
 
 
 
 function AboutMe() {
     return <div>
-       
-        <div className ="row">
+       <Card className="aboutMe" body outline>
+        <CardTitle tag="h2">Brandon Cermak - Full Stack Web Developer</CardTitle>
+        <Image src={linkedin1} width="200" height="200"></Image>
+        <CardText className = "aboutMe">I am currently in the process of a major career change after a very unexpected start to my 2020. Hospitality is/was my passion, however, there was always the idea in the back of my mind to make a major career shift to a more ‘technical’ occupation...after some motivation from the events of COVID19, I am ecstatic to be on my way to becoming a Full Stack Web Developer!</CardText>
+      </Card>
+        {/* <div className ="row">
             <div className ="col-md-12">
                 <div className = "card-body">
                 <h2 className = "knowMe"> Brandon Cermak - Full Stack Web Developer </h2>
@@ -16,7 +21,7 @@ function AboutMe() {
                 <p className = "aboutMe">I am currently in the process of a major career change after a very unexpected start to my 2020. Hospitality is/was my passion, however, there was always the idea in the back of my mind to make a major career shift to a more ‘technical’ occupation...after some motivation from the events of COVID19, I am ecstatic to be on my way to becoming a Full Stack Web Developer!</p>
                 </div>
             </div>
-        </div>
+        </div> */}
 
 </div>    
 }
