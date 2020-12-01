22b76e86e0d988e94c25d1c73dceb00af4afd3f7
Brandon Cermak <cermakprofessional@gmail.com>
2020-12-01 14:40:02 -0600

    changed contact page from a form to general contact cards

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,33 +1,29 @@
 import React from "react";
+import { Container, Card, CardTitle, CardText } from 'reactstrap';
 
 function Contact() {
-    return <div className="container">
-                <div className="row">
-                    <div className="col-md-12 formBody">
-                        <h3 className = "contactDirect"> Contact Direct </h3>
-                        <form className = "form1">
-                            <div class="form-group">
-                                <div class="form-group">
-                                <label for="orgSubmit">Organization</label>
-                                <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"></input>
-                                </div>
-                                <label for="nameSubmit">Name</label>
-                                <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"></input>
-                            </div>
-                            <div class="form-group">
-                                <label for="emailSubmit">Email Address</label>
-                                <input type="email" class="form-control" id="exampleInputPassword1"></input>
-                            </div>
-                            <div class="form-group">
-                                <label for="messageSubmit">Message</label>
-                                <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
-                            </div>
-                            <button type="submit" class="btn btn-primary">Submit</button>
-                        </form>
-                    </div>
-                    
-                </div>
-    </div>
+    return <Container classname ="contactContainer">
+    <Card className="contactContainer" body outline style={{backgroundColor: 'black', color: 'white'}}>
+      <CardTitle tag="h5"><i class="fas fa-phone-volume"> Call </i></CardTitle>
+      <CardText>(512)659-1685</CardText>
+    </Card>
+    <br></br>
+    <Card body outline style={{backgroundColor: 'black', color: 'white'}}>
+      <CardTitle tag="h5">Email</CardTitle>
+      <CardText>cermakprofessional@gmail.com</CardText>
+      <a href="mailto:cermakprofessional@gmail.com"><i class="fas fa-envelope"></i></a>
+    </Card>
+    <br></br>
+    <Card body outline style={{backgroundColor: 'black', color: 'white'}}>
+      <CardTitle tag="h5">LinkedIn</CardTitle>
+      <CardText><a href="https://www.linkedin.com/in/brandon-cermak-ab4219121/"><i class="fab fa-linkedin"></i></a></CardText>
+    </Card>
+    <br></br>
+    <Card body outline style={{backgroundColor: 'black', color: 'white'}}>
+      <CardTitle tag="h5">GitHub</CardTitle>
+      <CardText><a href="https://github.com/bcermak/"><i class="fab fa-github"></i></a></CardText>
+    </Card>
+  </Container>
 }
 
 export default Contact;
