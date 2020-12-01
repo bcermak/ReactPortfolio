d9710d41ea1de1548985f1e43ce9a6d63258f80a
Brandon Cermak <cermakprofessional@gmail.com>
2020-11-30 23:05:22 -0600

    working on trying to size the iframe/resume

--- src/pages/resume.js
+++ src/pages/resume.js
@@ -9,12 +9,10 @@ function Resume() {
     <Container>
         <Row>
             <Col>
-            <div className="resumeBody">
-    <div class="resume">
-        <a href = "https://drive.google.com/file/d/1UXcb3RkGwaURCepCkIGigpU1LpcrtxOR/view?usp=sharing"><h2>Resume</h2></a>
-        <iframe className = "resume" src="https://docs.google.com/document/d/e/2PACX-1vRBmroBKYj4E5wLh9ADc47_yEaJ5W0RqRqrv-XEYQYXtTFkGmBonLyH-be3eXlGrw/pub?embedded=true"></iframe>
-    </div>   
-    </div>
+                <div class="resume">
+                    <a href = "https://drive.google.com/file/d/1UXcb3RkGwaURCepCkIGigpU1LpcrtxOR/view?usp=sharing"><h2>Resume</h2></a>
+                    <iframe title="myResume" className = "resume" src="https://docs.google.com/document/d/e/2PACX-1vRBmroBKYj4E5wLh9ADc47_yEaJ5W0RqRqrv-XEYQYXtTFkGmBonLyH-be3eXlGrw/pub?embedded=true"></iframe>
+                </div>     
             </Col>
         </Row>
     </Container>
