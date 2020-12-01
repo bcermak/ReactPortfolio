0c50a89683026f1f7e082a8820a75723876061c1
Brandon Cermak <cermakprofessional@gmail.com>
2020-11-30 18:22:44 -0600

    added remaining images to portfolio page

--- src/pages/portfolio.js
+++ src/pages/portfolio.js
@@ -1,16 +1,25 @@
 import React from "react";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Image from 'react-bootstrap/Image'
+import KeepAustinSocial from './Assets/Images/keepaustinsocial.jpg'
+import BrewsNDadJokes from './Assets/Images/brewsndadjokes.jpg'
+import Scheduler from './Assets/Images/scheduler.jpg'
+import WeatherApp from './Assets/Images/WeatherApp.png'
+import EmpManager from './Assets/Images/empmanager.jpg'
+import PasswordGenerator from './Assets/Images/passwordgenerator.png'
 
 function Portfolio() {
     return <div className= "container">
         <div className ="row projects">
             <div className= "col-md-6 primaryProject">
-                        <a href = "https://enigmatic-brushlands-64346.herokuapp.com/"> Project 1 </a>
-                        <a href = "https://github.com/Moon-Ingenium/KeepAustinSocial"> GitHub </a>
+                        <div><a href = "https://enigmatic-brushlands-64346.herokuapp.com/"> Project 1 </a></div>
+                        <Image src={KeepAustinSocial} className ="primaryPic1"></Image>
+                        <div><a href = "https://github.com/Moon-Ingenium/KeepAustinSocial"> GitHub </a></div>
             </div>
             <div className="col-md-6 secondaryProject">
-                <a href = "https://bcermak.github.io/BrewsNDadJokes/"> Project 2 </a>
-                <img src="/Homework2/Assets/brews_screenshot.JPG" alt=""></img>
-                <a href = "https://github.com/bcermak/BrewsNDadJokes"> GitHub </a>
+                <div><a href = "https://bcermak.github.io/BrewsNDadJokes/"> Project 2 </a></div>
+                <Image src={BrewsNDadJokes} className ="primaryPic1"></Image>
+                <div><a href = "https://github.com/bcermak/BrewsNDadJokes"> GitHub </a></div>
             </div>
         </div>    
             <div className="row homeworks">
@@ -18,28 +27,28 @@ function Portfolio() {
                     <div className="media-body">
                     <h5 className="projectTitle"><a className = "projectTitle" href= "https://bcermak.github.io/Work-Day-Scheduler/"> Work Day Scheduler </a></h5>
                     </div>
-                    <img src="/Homework2/Assets/Images/scheduler.JPG" alt="screenshot"></img>
+                    <Image src={Scheduler}></Image>
                     <button className = "hwBtn1">Visit Repository</button>
                 </div> 
                 <div class="col-md-3 projectB">  
                     <div class="media-body">
                     <h5 class="projectTitle"><a class = "projectTitle" href= "https://bcermak.github.io/Weather-Dashboard/"> Weather Dashboard </a></h5>
                     </div>
-                    <img src="/Homework2/Assets/weather_screenshot.PNG" alt="screenshot"></img>
+                    <Image src={WeatherApp}></Image>
                     <button class = "hwBtn2">Visit Repository</button>
                 </div>
                 <div class="col-md-3 projectC">
                     <div class="media-body">
                     <h5 class="projectTitle"><a class = "projectTitle" href= "https://github.com/bcermak/employeetracker"> Employee Management System </a></h5>
                     </div>
-                    <img src="/Homework2/Assets/Images/empmanager.jpg"alt="soon"></img>
+                    <Image src={EmpManager}></Image>
                     <button class = "hwBtn3">Visit Repository</button>
                 </div>
                 <div class="col-md-3 projectD">
                     <div class="media-body">
                     <h5 class="projectTitle"><a class = "projectTitle" href= "https://bcermak.github.io/PasswordGenerator/"> Password Generator </a></h5>
                     </div>
-                    <img src="/Homework2/Assets/03-javascript-homework-demo.png"alt="soon"></img>
+                    <Image src={PasswordGenerator}></Image>
                     <button class = "hwBtn4">Visit Repository</button>
                 </div> 
                     </div>        
