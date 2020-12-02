6f00e1d99ca9bd9f11c13d690987e0c8da85cd81
Brandon Cermak <cermakprofessional@gmail.com>
2020-12-01 21:05:59 -0600

    changing font for aboutme

--- src/pages/aboutme.js
+++ src/pages/aboutme.js
@@ -9,16 +9,16 @@ import { Card, CardTitle, Container, Row, Col } from 'reactstrap';
 function AboutMe() {
     return <div>
        <Card style={{ backgroundColor:"black" }} className="aboutMe" body outline>
-        <CardTitle tag="h2">Brandon Cermak - Full Stack Web Developer</CardTitle>
+        <CardTitle style={{ textAlign:"center" }} tag="h2">Brandon Cermak - Full Stack Web Developer</CardTitle>
         <Container>
             <Row>
-                <Col className= ".col-3">
+                <Col >
                     <Image className="linkedin" src={linkedin1} width="200" height="200"></Image>
                 </Col>
-                <Col className= ".col-9">
-                    <div>Brandon is a web developer with an extensive background in Operational Management. A willingness to do any task to get the job done allowed Brandon to rise to the top of the ranks in hotel management, and this willingness will allow him to thrive in any role at your company!</div>
+                <Col >
+                    <div className="aboutText">Brandon is a web developer with an extensive background in operational management. A willingness to do any task to get the job done allowed Brandon to rise to the top of the ranks in hotel management, and this willingness will allow him to thrive in any role at your company!</div>
                     <br></br>  
-                    <div>2020 has been a year of surprises for us all. Brandon seized the chance to turn these surprises into growth and is excited to make the long-awaited leap into a new career!</div>
+                    <div className="aboutText">2020 has been a year of surprises for us all. Brandon seized the chance to turn these surprises into growth and is excited to make the long-awaited leap into a new career!</div>
                 </Col>    
             </Row>    
         </Container>
